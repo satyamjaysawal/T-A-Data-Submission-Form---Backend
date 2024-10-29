09efa8435f3d77ede481924fb76fb3226b1e321e
Satyam_jaysawal <sjrecm9258@gmail.com>
2024-10-29 12:42:12 +0530

    first c

--- app.py
+++ app.py
@@ -10,10 +10,6 @@ app.config["MONGO_URI"] = os.getenv("MONGO_URI", "mongodb+srv://todo:todo@todo.j
 mongo = PyMongo(app)
 db = mongo.db
 
-@app.route('/')
-def home():
-    return jsonify({"message": "Server is running"}), 200
-
 @app.route('/submit', methods=['POST'])
 def submit_data():
     data = request.json
@@ -25,5 +21,11 @@ def get_submissions():
     submissions = list(db.submissions.find({}, {'_id': 0}))  
     return jsonify(submissions), 200
 
+# Add a route to check if the server is running
+@app.route('/')
+def index():
+    return "Server is running!"
+
+# Only needed for local testing; Vercel doesn't use __main__
 if __name__ == '__main__':
     app.run(debug=True)
